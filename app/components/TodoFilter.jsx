d52f97ae97df37c210cf1751a8b3c18d2434a559
Russell Shurts <russell.w.shurts@gmail.com>
2016-09-28 13:44:37 -0500

    Update version and fix linting error

--- app/components/TodoFilter.jsx
+++ app/components/TodoFilter.jsx
@@ -4,15 +4,15 @@ import FilterLink from '../containers/FilterLink';
 const TodoFilter = () => (
   <p>
     Show:
-    {" "}
+    {' '}
     <FilterLink filter="SHOW_ALL">
       All
     </FilterLink>
-    {", "}
+    {', '}
     <FilterLink filter="SHOW_ACTIVE">
       Active
     </FilterLink>
-    {", "}
+    {', '}
     <FilterLink filter="SHOW_COMPLETED">
       Completed
     </FilterLink>
